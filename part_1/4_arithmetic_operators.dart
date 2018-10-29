f2c83c3bcd51a20f17809ea592066d04e5e5c7e4
Joseph S <38223151+Jodastt@users.noreply.github.com>
2018-10-28 17:53:15 -0700

    Create 4_arithmetic_operators.dart

---
+++ part_1/4_arithmetic_operators.dart
@@ -0,0 +1,20 @@
+main() {
+ 	
+  //defining variables
+  var num1 = 5;
+  var num2 = 10;
+  
+  //operator variables
+  var addition = num1 + num2;  //15
+  var subtraction = num1 - num2; //-5
+  var multiplication = num1 * num2; //50
+  var division = num1 / num2; // 0.5
+  var modulo = num1 % num2; //0
+  
+  print("num1 = $num1, num2 = $num2, addition = $addition, subtraction = $subtraction,         $multiplication, division = $division, modulo = $modulo");
+  
+  //combinations of operations: surface area of a cube
+  var side = 5;
+  var surfaceArea = 6 * pow(side, 2);
+  print(surfaceArea);
+}
